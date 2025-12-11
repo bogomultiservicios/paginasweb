bef7bcd1be65167e2d4a881bfe1a3fc6f2e2157c
bogomultiservicios <bogo.multiservicios@gmail.com>
2025-12-10 23:10:06 -0600

    PRUEBA SITIO 1
    PRUEBA DE PROYECTO EN GITHUB

---
+++ New folder/documento.js
@@ -0,0 +1,8 @@
+document.write("hola a todos!!");
+var nombre;
+nombre="<h1>soy Edwin</h1>";
+document.writeln(nombre);
+let nombre1, edad;
+nombre1 = prompt("Ingrese su nombre");
+edad = prompt("Ingrese su edad");
+document.write(nombre1, ' ', edad);
